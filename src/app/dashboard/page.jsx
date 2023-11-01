d018429126e23905d7ffcfe08ab4baf042a47319
Vicky AS <vickyhuang808@gmail.com>
2023-11-01 15:32:57 +0700

    reset project

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,51 +1,12 @@
 "use client"
 
-import Cookies from "js-cookie";
-import Router from "next/router";
+import { useState } from "react";
 
 export default function Dashboard() {
-    const token = Cookies.get('token');
+   
     const [user, setUser] = useState({});
-    const fetchData = async () => {
-
-        //set axios header dengan type Authorization + Bearer token
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-        //fetch user from Rest API
-        await axios.get(`${process.env.NEXT_PUBLIC_API_BACKEND}/api/user`)
-            .then((response) => {
-
-                //set response user to state
-                setUser(response.data);
-            })
-    }
-    useEffect(() => {
-
-        //check token empty
-        if (!token) {
-
-            //redirect login page
-            Router.push('/login');
-        }
-
-        //call function "fetchData"
-        fetchData();
-    }, []);
-
-    const logoutHanlder = async () => {
-
-        //set axios header dengan type Authorization + Bearer token
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-        //fetch Rest API
-        await axios.post(`${process.env.NEXT_PUBLIC_API_BACKEND}/api/logout`)
-        .then(() => {
-
-            //remove token from cookies
-            Cookies.remove("token");
-
-            //redirect halaman login
-            Router.push('/login');
-        });
-    };
+    
+    
     return (
 
         <div>
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,89 +1,24 @@
 "use client"
 
-import Buttons from '@/components/Buttons';
-import { useEffect, useState } from 'react';
-import Cookies from 'js-cookie';
-import axios from 'axios';
-import Router from 'next/router';
+import { useRouter } from 'next/router';
+import {useState} from 'react';
 
 export default function Login() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const router = useRouter();
 
-    const [valid, setValid] = useState([]);
+    const submit = async (e) => {
 
-    const loginHandler = async (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-
-        formData.append('email', email);
-        formData.append('password', password);
-
-        let object = {};
-        formData.forEach((value, key) => object[key] = value);
-        let json = JSON.stringify(object);
-        // console.log(json);
-
-        // const result = await axios.post(`${process.env.NEXT_PUBLIC_API_BACKEND}/api/login.php`,json,customConfig)
-
-        // console.log(result.data.data);
-
-        await axios.post(`${process.env.NEXT_PUBLIC_API_BACKEND}/api/login`, json)
-            .then((response) => {
-
-                //set token on cookies
-                Cookies.set('token', response.data.json);
-
-                //redirect to dashboard
-                Router.push('/dashboard');
-            })
-            .catch((error) => {
-
-                //assign error to state "validation"
-                setValid(error.response.data);
-            })
-
-    };
+    }
 
 
-    useEffect(() => {
-        if (Cookies.get('token')) {
-            console.log('get token');
-            
-        }
-    });
 
 
     return (
+        <main>
 
-
-        <div>
-            <h1>Log-in</h1>
-            <Buttons onClick={() => alert("Tidak dapat melakukan login untuk sementara. HTTP 422")}
-                text="Info error" />
-            <form onSubmit={loginHandler}>
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Masukkan Alamat Email" />
-                {
-                    valid.email && (
-                        <div>
-                            {valid.email[0]}
-                        </div>
-                    )
-                }
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Masukkan Password" />
-
-                {
-                    valid.password && (
-                        <div>
-                            {valid.password[0]}
-                        </div>
-                    )
-                }
-                <input type="submit" value="login" />
-
-            </form>
-
-        </div>
+        </main>
     );
 }
